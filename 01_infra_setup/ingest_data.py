8631ab920143db2f08387bc1f4141ef98bc4c6ed
gouthamas001 <asgoutham31@gmail.com>
2023-09-18 20:42:29 +1000

    changed folder structure and added readme file

---
+++ 01_infra_setup/ingest_data.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import os
+import pyarrow.parquet as pq
+from time import time
+from sqlalchemy import create_engine
+import argparse
+
+
+def trigger_ingestion(params):
+    # df = pd.read_parquet("yellow_tripdata_2023-01.parquet", nrows = 100000)
+    # print(pd.io.sql.get_schema(df, name = "yellow_taxi_ddl"))
+    # print(pd.io.sql.get_schema(df, name = "yellow_taxi", con=engine))
+    # df.to_sql(name="yello_taxi_data", con=engine, if_exists='replace')
+
+    # Read params from argparser object
+    user = params.user_name
+    password = params.password
+    host = params.host_name
+    port = params.port
+    db = params.db_name
+    table_name = params.table_name
+    url = params.url
+    input_file = "sample_2023.parquet"
+
+    # download the parquet file here
+    os.system(f"wget {url} -O {input_file}")
+
+    # Reading parquet file using pyarrow
+    parquet_file = pq.ParquetFile(input_file)
+
+    # Creating db connection using sqlalchemy
+    engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{db}")
+
+    counter = 1
+    for batch in parquet_file.iter_batches(batch_size=100000):
+        t_start = time()
+        batch_df = batch.to_pandas()
+        batch_df.to_sql(name=table_name, con=engine, if_exists="append")
+        t_end = time()
+        print(f"inserted chunk {counter}, took %.3f seconds" % (t_end - t_start))
+        counter += 1
+
+
+if __name__ == "__main__":
+    # Params required --> pg hostname, username, password, port, database name, table name , file url
+    parser = argparse.ArgumentParser(description="Ingest parquet file to postgres")
+
+    parser.add_argument("--user_name", help="user name for postgres")
+    parser.add_argument("--password", help="password for postgres")
+    parser.add_argument("--host_name", help="host name of postgres")
+    parser.add_argument("--port", help="postgres port")
+    parser.add_argument("--db_name", help="database name for postgres")
+    parser.add_argument("--table_name", help="table name for postgres")
+    parser.add_argument("--url", help="parquet file path to to download")
+
+    args = parser.parse_args()
+
+    trigger_ingestion(args)
---
+++ readme.md
@@ -0,0 +1,10 @@
+Another project to explore the data engineering techstack on google cloud
+
+Step 1 - Local testing
+
+1. Spawn docker containers for postgres and pgadmin using docker-compose.yaml
+2. Developed sample ingestion script to load parquet file into postgres db
+3. Deployed python ingestion sctipt into a docker container with python runtime env
+4. Triggerd the ingestion by providing commandline args
+
+   Technologies - Docker, Postgres, Python, Jupyter
