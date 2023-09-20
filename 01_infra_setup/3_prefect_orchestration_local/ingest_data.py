ed2b9c87de12397db0b25ec097fb8f77c350844d
gouthamas001 <asgoutham31@gmail.com>
2023-09-20 22:07:40 +1000

    added prefect tasks for extraction, transformation and load components

--- 01_infra_setup/3_prefect_orchestration_local/ingest_data.py
+++ 01_infra_setup/3_prefect_orchestration_local/ingest_data.py
@@ -5,10 +5,54 @@ import pyarrow.parquet as pq
 from time import time
 from sqlalchemy import create_engine
 from prefect import flow, task
+from prefect.tasks import task_input_hash
+from datetime import timedelta
+import pyarrow as pa
 
 
-@task()
-def trigger_ingestion(params):
+@task(
+    log_prints=True,
+    retries=3,
+    cache_key_fn=task_input_hash,
+    cache_expiration=timedelta(days=1),
+)
+def extract_data(url):
+    input_file = "sample_2023.parquet"
+    # input check
+    if url.endswith(".parquet"):
+        pass
+    else:
+        sys.exit()
+
+    # download the parquet file here
+    os.system(f"wget {url} -O {input_file}")
+
+    # Reading parquet file using pyarrow
+    parquet_file = pq.ParquetFile(input_file)
+
+    test_data = next(parquet_file.iter_batches(batch_size=100000))
+    df = pa.Table.from_batches([test_data]).to_pandas()
+
+    return df
+
+
+@task(log_prints=True)
+def transform(df):
+    print(
+        f"pre: missing passenger count before filtering: {df['passenger_count'].isin([0]).sum()}"
+    )
+
+    df = df[df["passenger_count"] != 0]
+
+    print(
+        f"post: missing passenger count after filtering: {df['passenger_count'].isin([0]).sum()}"
+    )
+
+    return df
+
+
+@task(log_prints=True, retries=3)
+def trigger_ingestion(params, data):
     # df = pd.read_parquet("yellow_tripdata_2023-01.parquet", nrows = 100000)
     # print(pd.io.sql.get_schema(df, name = "yellow_taxi_ddl"))
     # print(pd.io.sql.get_schema(df, name = "yellow_taxi", con=engine))
@@ -22,35 +66,27 @@ def trigger_ingestion(params):
     db = params["db_name"]
     table_name = params["table_name"]
     url = params["url"]
-    input_file = "sample_2023.parquet"
-
-    # input check
-
-    if url.endswith(".parquet"):
-        pass
-    else:
-        sys.exit()
-
-    # download the parquet file here
-    os.system(f"wget {url} -O {input_file}")
-
-    # Reading parquet file using pyarrow
-    parquet_file = pq.ParquetFile(input_file)
 
     # Creating db connection using sqlalchemy
     engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{db}")
+    data.to_sql(name=table_name, con=engine, if_exists="append")
 
-    try:
-        counter = 1
-        for batch in parquet_file.iter_batches(batch_size=100000):
-            t_start = time()
-            batch_df = batch.to_pandas()
-            batch_df.to_sql(name=table_name, con=engine, if_exists="append")
-            t_end = time()
-            print(f"inserted chunk {counter}, took %.3f seconds" % (t_end - t_start))
-            counter += 1
-    except StopIteration:
-        print("Failed ingestion!!!")
+    # try:
+    #     counter = 1
+    #     for batch in parquet_file.iter_batches(batch_size=100000):
+    #         t_start = time()
+    #         batch_df = batch.to_pandas()
+    #         batch_df.to_sql(name=table_name, con=engine, if_exists="append")
+    #         t_end = time()
+    #         print(f"inserted chunk {counter}, took %.3f seconds" % (t_end - t_start))
+    #         counter += 1
+    # except StopIteration:
+    #     print("Failed ingestion!!!")
+
+
+@flow(name="subflow", log_prints=True)
+def tes_subflow(table_name: str):
+    print(f"Testing subflow of prefect {table_name}")
 
 
 @flow(name="Test ingest flow")
@@ -77,7 +113,12 @@ def main():
         "table_name": "yellow_taxi_data",
         "url": "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-01.parquet",
     }
-    trigger_ingestion(conn_dic)
+
+    tes_subflow(conn_dic["table_name"])
+    sample_raw_data = extract_data(conn_dic["url"])
+    transformed_data = transform(sample_raw_data)
+
+    trigger_ingestion(conn_dic, transformed_data)
 
 
 if __name__ == "__main__":
